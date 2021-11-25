2aaa622c6b481dd39a853b94b7d3ad4df6ec3f69
Rohit Singh <rohitjsingh974@gmail.com>
2021-11-26 00:33:39 +0530

    add breadth-first-search and depth-first-search

--- src/algorithms/a-star.js
+++ src/algorithms/a-star.js
@@ -1,4 +1,4 @@
-import { sortOpenList, findNeighbours, getPath } from "./best-first-search";
+import { sortOpenList, findNeighbours } from "./best-first-search";
 
 export function aStar(grid, start, goal) {
     const openList = [];
@@ -28,38 +28,7 @@ export function aStar(grid, start, goal) {
                 neighbour.parent = current;
             }
         }
-        // console.log(openList.slice());
     }
-
-    // function sortOpenList() {
-    //     openList.sort((spotA, spotB) => spotA.fScore - spotB.fScore);
-    // }
-
-    // function findNeighbours(spot) {
-    //     spot.neighbors = [];
-    //     const {row, col} = spot;
-    //     if (row > 0) spot.neighbors.push(grid[row - 1][col]);
-    //     if (row < grid.length - 1) spot.neighbors.push(grid[row + 1][col]);
-    //     if (col > 0) spot.neighbors.push(grid[row][col - 1]);
-    //     if (col < grid[0].length - 1) spot.neighbors.push(grid[row][col + 1]);
-    // }
 
     return closedList;
 }
-
-// function removeFromArray(array, element) {
-//     for (let i = array.length - 1; i >= 0; i--) {
-//         if (array[i] === element) {
-//             array.splice(i, 1);
-//         }
-//     }
-// }
-
-// export function getPath(spot) {
-//     const path = [];
-//     while (spot.parent != null) {
-//         path.push(spot);
-//         spot = spot.parent;
-//     }
-//     return path;
-// }
